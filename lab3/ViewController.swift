ce0c615204875591b4def9308ed626671539d2cc
RaiSuzuse <raisuzuse@gmail.com>
2023-11-18 21:49:21 -0500

    Initial Commit

---
+++ lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by Suzuse Rai on 11/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
